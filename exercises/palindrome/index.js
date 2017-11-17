c948cc49bcdb832c076faad8a7015b817be58689
wb-qcy313750 <wb-qcy313750@antfin.com>
2017-11-17 13:59:48 +0800

    palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,10 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    return str.split('').every( (char, i) => {
+        return char === str[str.length - i - i]
+    })
+}
 
 module.exports = palindrome;
