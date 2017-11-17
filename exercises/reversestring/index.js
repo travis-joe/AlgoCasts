f350e4802a680997ca6b885c18788a3491e8cb43
qzy <weelgod@gmail.com>
2017-11-17 08:37:25 +0800

    reverseString

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,23 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+
+// function reverse(str) {
+//   return str.split('').reverse().join('')
+// }
+
+
+// function reverse(str) {
+//   let reversed = ''
+  
+//   for(let characters of str) {
+//     reversed = characters + reversed
+//   }
+//   return reversed
+// }
+
+function reverse(str) {
+  return str.split('').reduce( (reversed, char) =>  char + reversed, '')
+}
 
 module.exports = reverse;
