8e6b6980aafb6fdc2f74f07636542113bf054e88
qzy <weelgod@gmail.com>
2017-11-18 20:50:24 +0800

    save

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,24 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  helper(1)
+
+  function helper(a) {
+    if (a % 15 === 0) {
+      console.log('fizzbuzz')
+    } else if (a % 5 === 0) {
+      console.log('buzz')
+    } else if (a % 3 === 0) {
+      console.log('fizz')
+    } else {
+      console.log(a)
+    }
+    if (a < n) {
+      helper(a + 1)
+    }
+  }
+
+}
 
 module.exports = fizzBuzz;
