223b3d5542389db1cedaabbbf77e2ddf60ffaeb4
qzy <weelgod@gmail.com>
2017-11-25 21:16:57 +0800

    fibs

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,35 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// function fib(n) {
+//   const result = [0, 1];
+//   for (let i = 2; i <= n; i++) {
+//     const a = result[i - 1];
+//     const b = result[i - 2];
+//
+//     result.push(a + b);
+//   }
+//   return result[n];
+// }
 
+function slowFib(n) {
+  if (n < 2) {
+    return n
+  } else {
+    return fib(n - 1) + fib(n - 2)
+  }
+}
+
+
+function memoize(fn) {
+  const cache = {}
+  return function (...args) {
+    if(!cache[args]){
+      cache[args] = fn.apply(this, args)
+    }
+    return cache[args]
+  }
+}
+
+const fib = memoize(slowFib)
 module.exports = fib;
--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -32,22 +32,18 @@
 //   }
 // }
 
-function steps(n, row =0, stair = '') {
-  if(n === row) {
+function steps(n, row = 0, stair = '') {
+  if (n === row) {
     return
   }
 
-  if(n === stair.length) {
+  if (n === stair.length) {
     console.log(stair)
-    return steps(n, row+1)
-  }
-  if(stair.length <= row) {
-    stair += '#'
-  }else {
-    stair += ' '
+    return steps(n, row + 1)
   }
 
-  steps(n,row,stair)
+  const add = stair.length <= row ? '#' : ' '
+  steps(n, row, stair+add)
 }
 
 module.exports = steps;
