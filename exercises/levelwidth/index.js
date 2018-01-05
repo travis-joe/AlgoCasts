0cf828a9a31aa7d5dd33a6055543b94df22e75ba
qzy <weelgod@gmail.com>
2018-01-05 08:35:33 +0800

    levelWidth

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,25 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  //'s'--stop
+  const arr = [root, 's'];
+  const counters = [0];
+
+  while(arr.length > 1) {
+    const node = arr.shift()
+    //碰到s。加一个counters[0],s放到结尾
+    if(node === 's') {
+      counters.push(0)
+      arr.push('s')
+    } else {
+      //碰到node，添加子，counter最后+1
+      arr.push(...node.children);
+      counters[counters.length - 1] ++
+    }
+  }
+
+  return counters
+}
 
 module.exports = levelWidth;
